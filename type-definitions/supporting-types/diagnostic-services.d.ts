381b14ae5a5de29e44a979be2c19c67021148689
J <jgray@blockchainfoundry.co>
2018-10-25 20:11:24 -0700

    Fix to call signature.

--- type-definitions/supporting-types/diagnostic-services.d.ts
+++ type-definitions/supporting-types/diagnostic-services.d.ts
@@ -1,4 +1,4 @@
 export default interface DiagnosticServices {
-    getAllRawRpcMethods: Promise<Array<{method:string,arguments:string}>>;
+    getAllRawRpcMethods(): Promise<Array<{method:string,arguments:string}>>;
     helpForRpcCommand({commandName}:{commandName:string}): Promise<any>;
 }
