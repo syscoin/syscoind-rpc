a55f858c2de74d06b8a9841bd5b79cc8adc07bfb
J <jgray@blockchainfoundry.co>
2018-10-26 17:26:19 -0700

    Change logAndReturn to not use rpcException for network.

--- src/error-handlers/non-specific-network-error-handler.js
+++ src/error-handlers/non-specific-network-error-handler.js
@@ -9,15 +9,7 @@ export default class NonSpecificNetworkErrorHandler {
         return rpcError.message === "Network Error";
     }
 
-    logAndReturn(rpcError) {
-
-        if (rpcError.message === "Network Error") {
-            throw new RpcException({
-                result: rpcError.message,
-                error: `syscoin-js encounted an unknown network error.  Please verify your Syscoin core instance is running version 3.2 or above.`,
-                code: -3000
-            })
-        }
+    logAndReturn() {
         let networkErrorResponse = this.createCustomErrorResponse(`Encountered an unknown network error.  Please verify that ${this.url} is active and that the Syscoin core instance running is version 3.2 or above.`);
         this.logger.logCustomErrorResponse(networkErrorResponse);
         return networkErrorResponse;
