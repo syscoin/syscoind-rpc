1e05bede98e8e8208ed94456532e587025c5ccfd
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-05-07 18:48:17 -0700

    miininig service types

--- type-definitions/supporting-types/mining-services.d.ts
+++ type-definitions/supporting-types/mining-services.d.ts
@@ -1,13 +1,12 @@
 export default interface MiningServices {
-
-    createAuxBlock({address}: {address:string}): Promise<any>;
-    getAuxBlock({blockHash, auxPow}: {blockHash?:string,auxPow?:string}): Promise<any>;
-    getBlockTemplate({blockTemplate}: {blockTemplate?:string}): Promise<any>;
-    getMiningInfo(): Promise<any>;
-    getPoolInfo(): Promise<any>;
-    getNetworkHashesPerSecond({numberOfBlocks, blockHeight} : {numberOfBlocks:number, blockHeight:number}): Promise<any>;
-    prioritiseTransaction({txid,priorityDelta,feeDeltaInSatoshis}: {txid:string, priorityDelta:number,feeDeltaInSatoshis:number}): Promise<any>;
-    submitAuxBlock({blockHash,auxPow}: {blockHash:string,auxPow:string}): Promise<any>;
-    submitBlock({hexDataToSubmit}: {hexDataToSubmit:string}): Promise<any>;
+  createAuxBlock({address}: { address: string }): Promise<any>;
+  getAuxBlock({blockHash, auxPow}: { blockHash?: string, auxPow?: string }): Promise<any>;
+  getBlockTemplate({blockTemplate}: { blockTemplate: string }): Promise<any>;
+  getMiningInfo(): Promise<any>;
+  getNetworkHashesPerSecond({numberOfBlocks, blockHeight}: { numberOfBlocks: number, blockHeight: number }): Promise<any>;
+  prioritiseTransaction({txid, dummy, feeDeltaInSatoshis}: { txid: string, dummy?: null, feeDeltaInSatoshis: number }): Promise<any>;
+  submitAuxBlock({blockHash, auxPow}: { blockHash: string, auxPow: string }): Promise<any>;
+  submitBlock({hexDataToSubmit}: { hexDataToSubmit: string }): Promise<any>;
+  submitHeader({hexdata}: { hexData: string }): Promise<any>;
 }
 
