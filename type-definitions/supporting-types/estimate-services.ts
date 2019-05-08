4fc1e61f79cbbd4d279955013d141b67de290aa9
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-05-07 18:39:54 -0700

    estimate service types

--- type-definitions/supporting-types/estimate-services.ts
+++ type-definitions/supporting-types/estimate-services.ts
@@ -1,12 +1,3 @@
 export default interface EstimateServices {
-     estimateFee({numberOfBlocks}: {numberOfBlocks:number}) : Promise<any>;
-     estimatePriority({numberOfBlocks}: {numberOfBlocks:number}): Promise<any>; 
-     estimateSmartFee({numberOfBlocks}: {numberOfBlocks:number}): Promise<any>;
-     estimateSmartPriority({numberOfBlocks}: {numberOfBlocks:number}): Promise<any>;
-     aliasNewEstimatedFee({aliasName,publicValue,transfersFlag,expireTimestamp,address,encPrivateKey,encPublicKey,witness}:
-            {aliasName: string, publicValue: string, transfersFlag: number, expireTimestamp: number,
-            address: string, encPrivateKey: string, encPublicKey: string, witness: string}) : Promise<any>;
-     aliasUpdateEstimatedFee({aliasName, publicValue, address, transfersFlag, expireTimestamp, encPrivateKey,
-                                        encPublicKey, witness}: {aliasName: string, publicValue: string, address: string, transfersFlag: number,
-                                        expireTimestamp: number, encPrivateKey: string, encPublicKey: string, witness: string}): Promise<any>;
+  estimateSmartFee({confTarget, estimateMode}: { confTarget: number, estimateMode?: string }): Promise<any>;
 }
