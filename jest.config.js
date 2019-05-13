8c58448514a2d6fb35bc73ea430be97e9bdb0733
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-05-13 18:01:20 -0400

    base tests

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,6 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverage: false,
-  mapCoverage: false,
+  collectCoverage: true,
+  mapCoverage: true,
 };
