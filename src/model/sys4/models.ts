1d54506dc297c9f3af4ba55ec23818fff05b170a
Dan Wasyluk <Dan Wasyluk 987186+dwasyluk@users.noreply.github.com>
2019-05-22 21:09:00 -0400

    Transaction models

--- src/model/sys4/models.ts
+++ src/model/sys4/models.ts
@@ -2,3 +2,5 @@ export * from './asset';
 export * from './assetInfoRequest';
 export * from './assetNewRequest';
 export * from './assetNewResponse';
+export * from './transaction';
+export * from './transactionDetailEntry';
---
+++ src/model/sys4/transaction.ts
@@ -0,0 +1,15 @@
+import { TransactionDetailEntry } from './transactionDetailEntry';
+
+export interface Transaction { 
+    amount?: string;
+    confirmations?: number;
+    blockhash?: string;
+    blockindex?: number;
+    blocktime?: number;
+    txid?: string;
+    walletconflicts?: Array<any>;
+    time?: number;
+    timereceived?: number;
+    details?: Array<TransactionDetailEntry>;
+    hex?: string;
+}
